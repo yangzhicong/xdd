3fb50a7dc5800ca6ac9606be71740b2be2bf4605
yangzhicong <78916531+yangzhicong@users.noreply.github.com>
2021-11-13 13:51:47 +0800

    Add files via upload

---
+++ conf/demo_reply.php
@@ -0,0 +1,62 @@
+return reply(
+    [
+        `早上好` => `good morning`
+        `你好` => `好个锤子`  
+        `.*牛啊.*` => `牛啊牛啊`
+        `.*教程.*` => `没有教程，学习要靠自己悟。`
+        `.*(群主|群主在不在|机器人).*` => `https://img.doutugou.net/2019/06/9150e4e5gy1g3pyj97xjrj20f20bg0td.jpg`
+    ]
+)
+
+return reply(      
+    [
+        `.*骚话.*` => `https://api.vvhan.com/api/sao`
+        `语录` => `https://api.ixiaowai.cn/ylapi/index.php`
+        `舔狗` => `https://api.ixiaowai.cn/tgrj/index.php`
+    ]
+    
+return reply(      
+    [
+        `登录` => `请输入手机号码______`           
+        `查询` => `请先获取cookie，把获取到的cookie发送给机器人再输入“查询”`
+        `公告` => `暂无`
+    ]
+    
+return reply(      
+    [
+        `风景` => `http://laosepi.org/gqpic.php`           
+        `动漫` => `https://api.lyiqk.cn/acgst`     
+        `美女` => `https://api.lyiqk.cn/purelady`
+        `姐姐` => `lsp,爬`          
+        `妹妹` => `https://laosepi.org/pic.php`         
+        `.*(老司机|lsp|老骚批|福利).*` => `http://3650000.xyz/random/?mode=66`
+    ]   
+    
+return reply(      
+    [
+        `京东菜单` => `               京东菜单 ———————————————    
+登录  丨  查询  丨  公告 ———————————————`
+     ]  
+)
+
+return reply(      
+    [
+        `娱乐菜单` => `               娱乐菜单 ———————————————     
+风景 | 动漫 | 美女 | 姐姐 | 妹妹
+
+福利 | 许愿 | 骚话 | 语录 | 舔狗 ———————————————`
+    ]
+)
+
+return reply(      
+    [
+        `菜单` => `               京东菜单 
+———————————————    
+登录  丨  查询  丨  公告 ———————————————
+               娱乐菜单 
+———————————————      
+风景|动漫|美女|姐姐|妹妹
+
+福利|许愿|骚话|语录|舔狗 ———————————————` 
+    ]  
+)
---
+++ models/jdc.go
@@ -0,0 +1,164 @@
+package models
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/beego/beego/v2/client/httplib"
+	"github.com/beego/beego/v2/core/logs"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+var SMSCodes map[string]chan string
+
+type jdcLogin struct {
+	Ckcount int    `json:"ckcount"`
+	Qlkey   string `json:"qlkey"`
+	Phone   string `json:"Phone"`
+}
+type SendSMSResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    struct {
+		Status   int `json:"status"`
+		Ckcount  int `json:"ckcount"`
+		Tabcount int `json:"tabcount"`
+	} `json:"data"`
+}
+type JdcResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    struct {
+		Status int `json:"status"`
+	} `json:"data"`
+}
+type VerifyCodeResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    struct {
+		Qlid      string `json:"qlid"`
+		Nickname  string `json:"nickname"`
+		Timestamp string `json:"timestamp"`
+		Remarks   string `json:"remarks"`
+		Qlkey     int    `json:"qlkey"`
+		Ckcount   int    `json:"ckcount"`
+		Tabcount  int    `json:"tabcount"`
+	} `json:"data"`
+}
+
+func JdcConfig() {
+	req := httplib.Get(fmt.Sprintf("%s/api/Config", Config.JDCAddress))
+	data, _ := req.String()
+	logs.Info(data)
+	//sender.Reply("获取容器NvJDC配置成功")
+}
+
+func JdcSendSMS(sender *Sender, phone string) error {
+	req := httplib.Post(fmt.Sprintf("%s/api/SendSMS", Config.JDCAddress))
+	req.Header("Content-Type", "application/json")
+	body, _ := json.Marshal(struct {
+		Phone string `json:"Phone"`
+		Qlkey int    `json:"qlkey"`
+	}{
+		Phone: phone,
+		Qlkey: 1,
+	})
+	req.Body(body)
+	rsp, err := req.Response()
+	if err != nil {
+		return err
+	}
+	data, err := ioutil.ReadAll(rsp.Body)
+	obj := SendSMSResponse{}
+	err = json.Unmarshal(data, &obj)
+	if err == nil {
+		if obj.Success {
+			sender.Reply("请输入验证码")
+		} else {
+			if obj.Message != "" {
+				sender.Reply(obj.Message)
+				logs.Info(obj.Message)
+			}
+			if obj.Data.Status == 666 {
+				logs.Info("安全验证")
+				JdcAutoCaptcha(sender, phone, 1)
+			}
+		}
+
+	} else {
+		sender.Reply("登录出现错误")
+	}
+	return nil
+}
+
+func JdcAutoCaptcha(sender *Sender, phone string, number int) {
+	sender.Reply(fmt.Sprintf("正在尝试第%d次认证******", number))
+	req := httplib.Post(fmt.Sprintf("%s/api/AutoCaptcha", Config.JDCAddress))
+	req.Header("Content-Type", "application/json")
+	body, _ := json.Marshal(struct {
+		Phone string `json:"Phone"`
+	}{
+		Phone: phone,
+	})
+	req.Body(body)
+	rsp, err := req.Response()
+	if err != nil {
+		return
+	}
+	data, err := ioutil.ReadAll(rsp.Body)
+	obj := JdcResponse{}
+	err = json.Unmarshal(data, &obj)
+	if err == nil {
+		if obj.Success || obj.Data.Status != 666 {
+			sender.Reply("安全认证成功 请输入验证码______")
+		} else if !obj.Success && number <= 5 {
+			logs.Info("验证失败")
+			time.Sleep(time.Millisecond * 1000)
+			JdcAutoCaptcha(sender, phone, number+1)
+		} else {
+			sender.Reply("安全认证失败，请联系管理员或者前往网页手动登录,网址 yzc.yangcong.ml:5701")
+		}
+	}
+}
+
+func JdcVerifyCode(phone string, code string, qq string) bool {
+	req := httplib.Post(fmt.Sprintf("%s/api/VerifyCode", Config.JDCAddress))
+	req.Header("Content-Type", "application/json")
+	body, _ := json.Marshal(struct {
+		Phone string `json:"Phone"`
+		Qlkey int    `json:"qlkey"`
+		Code  string `json:"Code"`
+		QQ    string `json:"QQ"`
+	}{
+		Phone: phone,
+		Qlkey: 0,
+		Code:  code,
+		QQ:    qq,
+	})
+	req.Body(body)
+	rsp, err := req.Response()
+	data, err := ioutil.ReadAll(rsp.Body)
+	obj := VerifyCodeResponse{}
+	err = json.Unmarshal(data, &obj)
+	if err == nil {
+		if obj.Success {
+			logs.Info(obj.Message)
+		}
+	}
+	return obj.Success
+}
+func GetInput() string {
+	//使用os.Stdin开启输入流
+	//函数原型 func NewReader(rd io.Reader) *Reader
+	//NewReader创建一个具有默认大小缓冲、从r读取的*Reader 结构见官方文档
+	in := bufio.NewReader(os.Stdin)
+	//in.ReadLine函数具有三个返回值 []byte bool error
+	//分别为读取到的信息 是否数据太长导致缓冲区溢出 是否读取失败
+	str, _, err := in.ReadLine()
+	if err != nil {
+		return err.Error()
+	}
+	return string(str)
+}
