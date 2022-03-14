c4d88a31111ca456ecd5b46ce9a3c372e6526364
yangzhicong <78916531+yangzhicong@users.noreply.github.com>
2022-03-14 22:18:47 +0800

    Update handle.go

--- models/handle.go
+++ models/handle.go
@@ -170,8 +170,8 @@ module.exports = cookies`, cookies))
 							Pool: "s",
 						}
 
-						time.Sleep(time.Minute * 30)
-						// time.Sleep(time.Second * 1)
+						// time.Sleep(time.Minute * 30)
+						time.Sleep(time.Second * 1)
 					}
 				}()
 				init = false
